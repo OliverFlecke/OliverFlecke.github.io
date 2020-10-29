508ebc037a05a13d1efe003aac3ce11fc2d8df55
Oliver Fleckenstein <oliverfl@live.dk>
2020-10-29 22:43:53 +0800

    Fixed issue with missing styling when pruning CSS

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
 module.exports = {
 	plugins: [
 		require('postcss-import'),
-		require('tailwindcss'),
 		require('postcss-nesting'),
+		require('tailwindcss'),
 		require('autoprefixer'),
 		require('cssnano'),
 		require('postcss-hash'),
