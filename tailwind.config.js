64850d0fd206a60df4360f22cb6ffe6ad652e5c3
Oliver Fleckenstein <oliverfl@live.dk>
2021-11-28 23:25:08 +0100

    style: Styling for app page

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,12 @@ module.exports = {
 		mode: 'all',
 		enable: true,
 		content: ['**/*.html'],
+		safelist: [
+			"dark:bg-rose-800",
+			"bg-rose-500",
+			"text-rose-700",
+			"dark:text-rose-600"
+		]
 	},
 	darkMode: 'media',
 	theme: { colors: { ...colors } },
