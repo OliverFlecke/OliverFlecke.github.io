f93134795978e712e982e8a13affbf6841463c4b
Oliver Fleckenstein <oliverfl@live.dk>
2026-06-10 10:18:15 +0200

    chore: update build dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,6 @@
-const colors = require('tailwindcss/colors');
-
 module.exports = {
-	mode: 'jit',
-	// purge: {
-	// 	mode: 'all',
-	// 	enable: true,
-	// 	content: ['**/*.html'],
-	// 	safelist: ['dark:bg-rose-800', 'bg-rose-500', 'text-rose-700', 'dark:text-rose-600'],
-	// },
-	content: ['**/*.html'],
+	mode: "jit",
+	content: ["(content|layouts)/**/*.html"],
 	variants: {},
 	plugins: [],
 };
